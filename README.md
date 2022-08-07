0bda6b2c49dea147007183852f3be9dc8421073b
Yanitsa Aleksieva <ianaaleksieva@gmail.com>
2022-08-07 22:49:58 +0300

    manage readMe

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # ShareIT
  a simple SPA developed with Express, Mongoose and React
+
+
+Technologies:
+
+ - Bootstrap 4 or 5
+ - React
+ - JavaScript
+ - Mongoose
+ - Express
+
+
+ Launch:
+
+You have to install MongoDB Compass on your local machine.
+
+Folders 
+
+
+/server 
+
+- Use the package manager npm to install all of the required dependencies. / npm i
+- Use the package manager npm to run the server by typing npm start.
+
+/client/share-it
+
+-  Use the package manager npm to install all of the required dependencies. / npm i
+-  Use the package manager npm to run the client by typing npm start.
--- client/share-it/src/components/Home/Home.js
+++ client/share-it/src/components/Home/Home.js
@@ -1,92 +1,69 @@
 import { Link } from "react-router-dom";
 import './home.css'
 
-const Home  = () => {
+const Home = () => {
 
 
-    return (
-<div className="container py-5">
-  <div className="row gutters">
-    <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
-      <div className="pricing-plan">
-        <div className="pricing-header">
-         
-          <div className="pricing-cost">Mathematics</div>
-         
-        </div>
-        <ul className="pricing-features">
-          <li>5GB Linux Web Space</li>
-          <li>5 MySQL Databases</li>
-          <li>500 Emails</li>
-          <li>250Gb mothly Transfer</li>
-          <li className="text-muted">
-            <del>24/7 Tech Support</del>
-          </li>
-          <li className="text-muted">
-            <del>Daily Backups</del>
-          </li>
-        </ul>
-        <div className="pricing-footer">
-          <Link to="/math" className="btn btn-primary btn-lg">
-           Try it
-          </Link>
-        </div>
-      </div>
-    </div>
-    <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
-      <div className="pricing-plan">
-        <div className="pricing-header">
-          
-          <div className="pricing-cost">English</div>
+  return (
+    <div className="container py-5">
+      <div className="row gutters">
+        <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+          <div className="pricing-plan">
+            <div className="pricing-header">
 
-        </div>
-        <ul className="pricing-features">
-          <li>10GB Linux Web Space</li>
-          <li>10 MySQL Databases</li>
-          <li>1000 Emails</li>
-          <li>750Gb mothly Transfer</li>
-          <li>24/7 Tech Support</li>
-          <li className="text-muted">
-            <del>Daily Backups</del>
-          </li>
-        </ul>
-        <div className="pricing-footer">
-          <a href="/enlish" className="btn btn-primary btn-lg">
-            Try it
-          </a>
-        </div>
-      </div>
-    </div>
-    <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 ">
-      <div className="pricing-plan bg">
-        <div className="pricing-header">
-          
-          <div className="pricing-cost">Bulgarian</div>
+              <div className="pricing-cost">Mathematics</div>
 
+            </div>
+            <ul className="pricing-features">
+
+            </ul>
+            <div className="pricing-footer">
+              <Link to="/math" className="btn btn-primary btn-lg">
+                Try it
+              </Link>
+            </div>
+          </div>
         </div>
-        <ul className="pricing-features">
-          <li>10GB Linux Web Space</li>
-          <li>10 MySQL Databases</li>
-          <li>1000 Emails</li>
-          <li>750Gb mothly Transfer</li>
-          <li>24/7 Tech Support</li>
-          <li className="text-muted">
-            <del>Daily Backups</del>
-          </li>
-        </ul>
-        <div className="pricing-footer">
-          <a href="/enlish" className="btn btn-primary btn-lg">
-            Try it
-          </a>
+        <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+          <div className="pricing-plan">
+            <div className="pricing-header">
+
+              <div className="pricing-cost">English</div>
+
+            </div>
+            <ul className="pricing-features">
+
+            </ul>
+            <div className="pricing-footer">
+              <a href="/enlish" className="btn btn-primary btn-lg">
+                Try it
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 ">
+          <div className="pricing-plan bg">
+            <div className="pricing-header">
+
+              <div className="pricing-cost">Bulgarian</div>
+
+            </div>
+            <ul className="pricing-features">
+
+            </ul>
+            <div className="pricing-footer">
+              <a href="/enlish" className="btn btn-primary btn-lg">
+                Try it
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
 
 
 
-    );
+  );
 }
 
 
