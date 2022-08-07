22a8715bbfc6406efd19814abd77093ed1e51212
Yanitsa Aleksieva <ianaaleksieva@gmail.com>
2022-08-07 09:04:09 +0300

    add my profile css and avatar

--- client/share-it/src/components/Profile/Profile.js
+++ client/share-it/src/components/Profile/Profile.js
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/authContext.js";
 import { editProfile, retrieveAvatar } from "../../services/userService.js";
+import './profile.css';
+
 
 const Profile = () => {
   const { user } = useContext(AuthContext);
@@ -24,10 +26,10 @@ console.log(img)
 
   return (
 
-    <section className=" vh-100 gradient-custom ">
+    <section className="gradient-custom ">
       <div className="container  py-5 h-50 ">
-        <div className="avatar">
-          <img src={img.src} class="rounded-circle"
+      <div className="avatar">
+          <img src={"https://xsgames.co/randomusers/assets/avatars/pixel/29.jpg"} class="rounded-circle"
             alt="Avatar" />
         </div>
         <form className="" onSubmit={onSubmit}>
@@ -39,7 +41,7 @@ console.log(img)
               >
                 <div className="card-body p-5 text-center">
                   <div className="mb-md-5 mt-md-4 pb-5">
-                    <h2 className="fw-bold mb-2 text-uppercase">My Profile</h2>
+                    <h2 className="fw-bold mb-2 text-uppercase my-profile">My Profile</h2>
                     <p className="text-white-50 mb-5">
                       Welcome, {user.name}!
                     </p>
@@ -88,7 +90,7 @@ console.log(img)
                       className="btn btn-outline-light btn-lg px-5"
                       type="submit"
                     >
-                      Register
+                      Update profile
                     </button>
                     <div className="d-flex justify-content-center text-center mt-4 pt-1">
                       <a href="#!" className="text-white">
