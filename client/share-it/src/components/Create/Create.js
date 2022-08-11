e1b0f3ccf9e15f13133cbbe866f226f077bbe35e
Yanitsa Aleksieva <ianaaleksieva@gmail.com>
2022-08-11 08:34:54 +0300

    update create test functionallity

--- client/share-it/src/components/Create/Create.js
+++ client/share-it/src/components/Create/Create.js
@@ -17,7 +17,7 @@ const Create = () => {
     data._ownerId = user._id;
     console.log(data);
     try {
-      
+
       const response = await createTest(data);
       console.log(response);
 
@@ -30,94 +30,99 @@ const Create = () => {
   }
   return (
 
-    <div className="container mt-5">
-      <form onSubmit={onSubmit}>
+    <div className="container mt-5 vh-100 gradient-custom py-5">
+      <form onSubmit={onSubmit} className="">
         <div className=" boreder d-flex justify-content-center row">
           <div className="col-md-10 col-lg-10">
-            <div className="border">
-              <div className="question text-dark p-3 border-bottom">
-                <div className="d-flex flex-row justify-content-between align-items-center mcq">
-                  <h4>Title</h4>
-                  <input
-                    className="ml-3"
-                    placeholder=""
-                    name="title"
+            <div className="card bg-dark text-white"
+              style={{ borderRadius: "1rem" }}>
+              <div className="card-body p-5 text-center">
+                <div className="question text-white p-3 border-bottom">
+                  <div className=" d-flex flex-row justify-content-between align-items-center mcq">
+                    <h4>Title</h4>
+                    <input
+                      className="ml-3"
+                      placeholder=""
+                      name="title"
+                    >
+                    </input>
+                    <h4>Class</h4>
+                    <input
+                      className="ml-3"
+                      name="class"
+                    >
+                    </input>
+                    <h4>Subject</h4>
+                    <select
+                      className="ml-3"
+
+                      name="subject"
+                    >
+                      <option value="mathematics">Mathematics</option>
+                      <option value="english">English</option>
+                      <option value="bulgarian">Bulgarian</option>
+                    </select>
+
+                  </div>
+
+                </div>
+                <div className="question bg-dark text-white p-3 border-bottom">
+                  <div className="d-flex flex-row align-items-center question-title">
+                    <h3 className="text-danger">1.</h3>
+                    <input
+                      className="ml-5"
+                      placeholder="Type your question"
+                      name="question1"
+                    >
+                    </input>
+                  </div>
+                  <div className="ans ml-2 text-white">
+                    <label >a.</label>
+
+                    <input className="ml-5"
+                      placeholder="answer"
+                      name="answer1" />
+                  </div>
+                  <div className="ans ml-2 text-white">
+                    <label >b. </label>
+
+                    <input className="ml-5 text-white"
+                      placeholder="answer"
+                      name="answer2" />
+
+
+
+                  </div>
+                  <div className="ans ml-2 text-white">
+                    <label >c.  </label>
+
+                    <input className="ml-5"
+                      placeholder="answer"
+                      name="answer3" />
+                  </div>
+                  <div className=" ml-2 text-white">
+                    <label >d. </label>
+
+                    <input className="ml-5"
+                      placeholder="answer"
+                      name="answer4" />
+                  </div>
+                </div>
+
+                <div className="footer d-flex flex-row justify-content-between align-items-rig p-3 bg-dark">
+
+                  <button
+                    className="create btn btn-primary border-success align-items-center btn-success"
+                    type="submit"
                   >
-                  </input>
-                  <h4>Class</h4>
-                  <input
-                    className="ml-3"
-                    name="class"
-                  >
-                  </input>
-                  <h4>Topic</h4>
-                  <select
-                    className="ml-3"
-
-                    name="subject"
-                  >
-                     <option value="mathematics">Mathematics</option>
-                        <option value="english">English</option>
-                        <option value="bulgarian">Bulgarian</option>
-                  </select>
+                    Create
+                    <i className="fa fa-angle-right ml-2" />
+                  </button>
 
                 </div>
-               
-              </div>
-              <div className="question bg-white p-3 border-bottom">
-                <div className="d-flex flex-row align-items-center question-title">
-                  <h3 className="text-danger">1.</h3>
-                  <input
-                    className="ml-5"
-                    placeholder="Type your question"
-                    name="question1"
-                  >
-                  </input>
-                </div>
-                <div className="ans ml-2">
-                  <label >a.</label>
-
-                  <input className="ml-5"
-                    placeholder="answer"
-                    name="answer1" />
-                </div>
-                <div className="ans ml-2">
-                  <label >b. </label>
-
-                  <input className="ml-5"
-                    placeholder="answer"
-                    name="answer2" />
-
-
-
-                </div>
-                <div className="ans ml-2">
-                  <label >c.  </label>
-
-                  <input className="ml-5"
-                    placeholder="answer"
-                    name="answer3" />
-                </div>
-                <div className=" ml-2">
-                  <label >d. </label>
-
-                  <input className="ml-5"
-                    placeholder="answer"
-                    name="answer4" />
-                </div>
-              </div>
-
-              <div className="footer d-flex flex-row justify-content-between align-items-rig p-3 bg-white">
-
-                <button
-                  className="create btn btn-primary border-success align-items-center btn-success"
-                  type="submit"
-                >
-                  Create
-                  <i className="fa fa-angle-right ml-2" />
-                </button>
               </div>
             </div>
+
           </div>
         </div>
       </form>
